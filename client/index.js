520c4771376a78b0bc48ba70ff80867458f97204
jamesonjackson <jjackson@newvisions.org>
2020-03-30 21:04:53 -0400

    initial load server-side but then toggle to a client-side model when a user has to perform batch actions

--- client/index.js
+++ client/index.js
@@ -1,70 +1,135 @@
 import 'ag-grid-enterprise';
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
 import { Grid } from 'ag-grid-community';
-import { HeaderCheckbox } from './header-checkbox.component';
-import { CellCheckbox } from './cell-checkbox.component';
-import { GroupCheckbox } from './group-checkbox.component';
 
-const gridOptions = {
-	rowModelType: 'serverSide',
-	rowSelection: 'multiple',
-	// ctx available to all cell renderers
-	context: {
-		headerState: 'UNCHECKED',
-		groupState: { },
-		selectedRows: { },
-		renderedRows:  { },
-		selectedGroups: new Set(),
-		renderedGroups: new Set(),
-	},
-	components: { 
-		agColumnHeader: HeaderCheckbox,
-		'cellCheckboxRenderer': CellCheckbox,
-	},
-	columnDefs: [
-		{ 
-			field: 'athlete', 
-			cellRenderer: 'cellCheckboxRenderer',
-		},
-		{
-			field: 'country',
-			rowGroup: true, 
-			hide: true
-		},
-		{ field: 'sport' },
-		{ field: 'year', filter: 'number' },
-		{ field: 'gold', aggFunc: 'sum' },
-		{ field: 'silver', aggFunc: 'sum' },
-		{ field: 'bronze', aggFunc: 'sum' },
-	],
-	autoGroupColumnDef: {
-		groupSelectsChildren: true,
-		cellRenderer:'agGroupCellRenderer',
-    cellRendererParams: {
-			innerRenderer: GroupCheckbox,
-    },
+// initialize in server mode
+let serverMode;
+document.addEventListener('DOMContentLoaded', function() { 
+	serverMode = 'SERVER'
+	createServerSideGrid();
+});
+
+function toggleGrid() {
+	if(serverMode === 'CLIENT') {
+		serverMode = 'SERVER'
+		createServerSideGrid();
+		return 
 	}
-};
+	if(serverMode === 'SERVER') {
+		serverMode = 'CLIENT'
+		createClientSideGrid()
+		return
+	}
+}
 
-const gridDiv = document.querySelector('#myGrid');
+function createServerSideGrid() {
+	const gridOptions = {
+		rowModelType: 'serverSide',
+		columnDefs: [
+			{ 
+				field: 'athlete', 
+				hide: true
+			},
+			{
+				field: 'country',
+				rowGroup: true, 
+				hide: true
+			},
+			{ field: 'sport' },
+			{ field: 'year', filter: 'number' },
+			{ field: 'gold', aggFunc: 'sum' },
+			{ field: 'silver', aggFunc: 'sum' },
+			{ field: 'bronze', aggFunc: 'sum' },
+		],
+		suppressAggFuncInHeader: true,
+		autoGroupColumnDef: {
+			cellRenderer:'agGroupCellRenderer',
+			headerName: 'Athlete'
+		}
+	};
 
-new Grid(gridDiv, gridOptions);
+	const gridDiv = document.querySelector('#myGrid');
+	
+	if(gridDiv.firstChild) gridDiv.firstChild.remove();
 
-const datasource = {
-	getRows(params) {
-		fetch('./olympicWinners/', {
-			method: 'post',
-			body: JSON.stringify(params.request),
-			headers: { "Content-Type": "application/json; charset=utf-8" }
-		})
-			.then(httpResponse => httpResponse.json())
-			.then(response => params.successCallback(response.rows, response.lastRow))
-			.catch(error => {
-				console.error(error);
-				params.failCallback();
+	new Grid(gridDiv, gridOptions);
+
+	const datasource = {
+		getRows(params) {
+			fetch('./olympicWinners/', {
+				method: 'post',
+				body: JSON.stringify(params.request),
+				headers: { "Content-Type": "application/json; charset=utf-8" }
 			})
-	}
-};
+				.then(httpResponse => httpResponse.json())
+				.then(response => params.successCallback(response.rows, response.lastRow))
+				.catch(error => {
+					console.error(error);
+					params.failCallback();
+				})
+		}
+	};
 
-gridOptions.api.setServerSideDatasource(datasource);
+	gridOptions.api.setServerSideDatasource(datasource);
+}
+
+function createClientSideGrid() {
+	const gridOptions = {
+		columnDefs: [
+			{ field: 'country', rowGroup: true, hide: true },
+			{ field: 'sport'},
+			{ field: 'year', },
+			{ field: 'gold', aggFunc: 'sum' },
+			{ field: 'bronze', aggFunc: 'sum' },
+			{ field: 'silver', aggFunc: 'sum' },
+		],
+		defaultColDef: {
+			flex: 1,
+			minWidth: 100,
+		},
+		autoGroupColumnDef: {
+			headerName: 'Athlete',
+			hide: true,
+			field: 'athlete',
+			cellRenderer: 'agGroupCellRenderer',
+			cellRendererParams: {
+				checkbox: true,
+			},
+		},
+		rowSelection: 'multiple',
+		groupSelectsChildren: true,
+		suppressRowClickSelection: true,
+		suppressAggFuncInHeader: true,
+	};
+	
+		const gridDiv = document.querySelector('#myGrid');
+
+		if(gridDiv.firstChild) gridDiv.firstChild.remove();
+		
+		new Grid(gridDiv, gridOptions);
+	
+		// do http request to get our sample data - not using any framework to keep the example self contained.
+		// you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
+		var httpRequest = new XMLHttpRequest();
+		httpRequest.open(
+			'GET',
+			'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinnersSmall.json'
+		);
+		httpRequest.send();
+		httpRequest.onreadystatechange = function() {
+			if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+				var httpResult = JSON.parse(httpRequest.responseText);
+				gridOptions.api.setRowData(httpResult);
+			}
+		};
+
+}
+
+// add button for poc
+document.addEventListener('DOMContentLoaded', function() {
+	const toggleBtn = document.querySelector('#grid-toggle');
+	toggleBtn.addEventListener('click', toggleGrid)
+ });
+
